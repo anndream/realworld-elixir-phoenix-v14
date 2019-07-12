b8f8b3d37099f5efe47c92d8c33b16763526eb7a
Anndream <anndream.com@gmail.com>
2019-07-12 15:33:07 +0700

    upgrade phoenix version to 1.4

---
+++ .formatter.exs
@@ -0,0 +1,24 @@
+[
+  # inputs: [
+  #   "mix.exs",
+  #   "{config,lib,test,priv}/**/*.{ex,exs}"
+  # ],
+  locals_without_parens: [
+    # Formatter tests
+    assert_format: 2,
+    assert_format: 3,
+    assert_same: 1,
+    assert_same: 2,
+
+    # Errors tests
+    assert_eval_raise: 3,
+
+    # Mix tests
+    in_fixture: 2,
+    in_tmp: 2
+  ],
+  [
+    import_deps: [:phoenix],
+    inputs: ["*.{ex,exs}", "{config,lib,priv,test}/**/*.{ex,exs}"]
+  ]
+]
